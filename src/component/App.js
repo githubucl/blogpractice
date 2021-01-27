c031a4d11323189c28e53ba53d3c84c10a327cf4
chris Chen <59293007+githubucl@users.noreply.github.com>
2021-01-27 14:28:47 +1300

    before going to massey

---
+++ src/component/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PostList from './PostList'
+const App = () => {
+    return (
+        <div className="ui ">
+            <PostList />
+        </div>
+    )
+}
+
+export default App
